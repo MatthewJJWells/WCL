4280d6f57d788d4a5d465042bc36ed69cc0c080e
Jokubas Markevicius <markeviciusjokubas@gmail.com>
2021-05-04 17:52:41 +0100

    added a new test

--- frontend/src/components/__test__/API.int.test.js
+++ frontend/src/components/__test__/API.int.test.js
@@ -19,7 +19,7 @@ describe('fetchRaideriodata tests', () => {
 			.catch(error => console.error(error));
 	});
 
-	it('Provided an incorrect realm or name, reroutes the user to a not found page', () => {
+	it('Provided an incorrect realm or name, returns an error response object', () => {
 		fetch.mockOnce(JSON.stringify(mockRealmError));
 		fetchRaiderioData({
 			server:'eu',
@@ -40,6 +40,20 @@ describe('fetchRaideriodata tests', () => {
 				expect(res.message).toBe('Could not find requested character');
 			})
 			.catch(error => console.error(error));
+	});
+
+	it('Creates the right url provided the searchDetails parameter', () => {
+		fetch.mockOnce(mockCharacterData);
+		fetchRaiderioData({
+			server: 'eu',
+			realm: 'tarren-mill',
+			name: 'airling'
+		});
+		expect(fetch).toHaveBeenCalledWith(`https://raider.io/api/v1/characters/profile
+		?region=eu
+		&realm=tarren-mill
+		&name=airling
+		&fields=gear%2Cguild%2Cmythic_plus_scores_by_season%3Acurrent%2Cmythic_plus_best_runs%2Craid_progression`);
 	});
 });
 
